210ab8224a4712b00cadbf02225546e92e0af304
mendelsshop <mendelsshop@gmail.com>
2023-08-29 12:27:56 -0400

    tokenhandler done

--- 3-Parser/src/TokenHandler.java
+++ 3-Parser/src/TokenHandler.java
@@ -1,7 +1,6 @@
 import java.util.LinkedList;
 import java.util.Optional;
 
-
 public class TokenHandler {
     private LinkedList<Token> tokenStream;
 
@@ -9,15 +8,27 @@ public class TokenHandler {
         this.tokenStream = tokenStream;
     }
 
-    public Optional<Token> MatchAndRemove(Token.TokenType t)  {
+    public Optional<Token> MatchAndRemove(Token.TokenType t) {
+        var peek = tokenStream.getFirst();
+        if (peek != null) {
+            if (peek.getType() == t) {
+                tokenStream.pop();
+                return Optional.of(peek);
+            }
+        }
         return Optional.empty();
     }
 
     public boolean MoreTokens() {
-        return false;
+        return !tokenStream.isEmpty();
     }
 
+    // index starts at 0
     public Optional<Token> Peek(int j) {
+        var peek = tokenStream.get(j);
+        if (peek != null) {
+            return Optional.of(peek);
+        }
         return Optional.empty();
     }
 }
